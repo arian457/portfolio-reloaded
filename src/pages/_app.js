b0eb45cad7fe8979692172b1c83153c39ca844e1
arian457 <ariancejas00@gmail.com>
2021-11-30 16:19:35 -0300

    imported global stylings for App

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import "../../public/styles/globals.css";
+import "../../public/fonts/fonts.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
